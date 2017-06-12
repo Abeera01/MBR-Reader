045f5d2db477d1f2997cd9fdb5dc03b392c65df5
Abeera Usman Malik <Abeera Usman Malik@192.168.111.1>
2017-06-12 21:21:57 +0500

    Final Code

--- ForensicTool/src/forensictool/ForensicTool.java
+++ ForensicTool/src/forensictool/ForensicTool.java
@@ -52,6 +52,9 @@ String[] parts=diskRead(0,driveName);//diskRead is a function that reads from di
 
 if((parts[450].equals("EE")) || (parts[450].equals("ee"))){
     System.out.println("The Drive does not have MBR partitioning Style. It has GPT Partitioning Style");
+    System.out.println("Press Enter to Exit");
+        System.in.read();
+        System.exit(0);
 }
 else{
         //Partition Details
